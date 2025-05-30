a9addb618cc5007173a2e5efab5220e146f881a5
Elhama Tokhi <tokhielaham30@gmail.com>
2025-05-31 01:48:45 +0200

    Setup the server

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import morgan from 'morgan'
+import router from './routes/index.js'
+import bodyParser from 'body-parser'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`)
+})
